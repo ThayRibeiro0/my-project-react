40e0e30208e445f2041718f7491e008f2e183188
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2023-08-12 16:57:55 -0700

    6

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,34 @@
-function Item(props) {
+// advanced props
+import Proptypes from 'prop-types';
+
+function Item({marca, ano_lancamento}) {
  return (
     <>
-        <li>{props.marca}</li>
-        {/* can I put more because he use it but its more to list with title and list and the fragment it's good */}
-        <p>Teste</p>
+        <li>
+            {marca} - {ano_lancamento}
+        </li>
     </>
  )
 }
 
+//sugesttion to next developer for do something
+Item.propTypes = {
+    marca: Proptypes.string.isRequired,
+    ano_lancamento: Proptypes.number,
+}
+
+Item.defaultProps = {
+    marca: "Faltou a marca",
+    ano_lancamento: 0,
+
+}
+
 export default Item
+
+// function Item(props) {
+
+// inside of fragment can I put more because he use it but its more to list with title and list and the fragment it's good
+//         <li>{props.marca}</li>
+//         <p>Teste</p>
+
+       
--- src/components/List.js
+++ src/components/List.js
@@ -1,21 +1,26 @@
-import Item from "./Item"; 
-// using the item component with one item
+import Item from "./Item"
 
 function List(){
     return (
-        // fragment exemplo that permit the creation of a component without element to facility the problems with DOM:
         <> 
             <h1>My list</h1>
             <ul>
-                {/* invocation for component Item */}
-                <Item marca="Ferrari" />
-                <Item marca="Fiat" />
+                <Item marca="Ferrari" ano_lancamento={1985}/>
+                <Item marca="Fiat" ano_lancamento={1964}/>
                 <Item marca="Renault" />
-                {/* <li>Item 1</li>
-                <li>Item 2</li> */}
+                <Item marca="Chevolet" ano_lancamento={1999} />
+                {/* default props insert one data already done in some component in this case Item */}
+                <Item /> 
             </ul>
         </>
     )
 }
 
 export default List;
+
+// using the item component with one item
+// <> </> FRAGMENT exemple that permit the creation of a component without element to facility the problems with DOM:
+// invocation for component in <li> Item 
+
+//                 <li>Item 1</li>
+//                 <li>Item 2</li>
